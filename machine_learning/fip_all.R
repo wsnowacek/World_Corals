8c3f8c25f6a8914b4ee11e1a905df12108de60ee
Henry Sun <hs325@duke.edu>
2026-01-27 00:23:47 -0500

    making new figs

--- machine_learning/fip_all.R
+++ machine_learning/fip_all.R
@@ -21,7 +21,6 @@ merged_df_all <- feature_importance_comparison_all %>%
   inner_join(met_df, by = "metabolite")
 
 
-
 ################################################################################
 
 target_classes <- trimws(c(
@@ -46,14 +45,15 @@ process_importance_data <- function(df, importance_col) {
     mutate(metabolite = fct_reorder(metabolite, !!sym(importance_col), .desc = TRUE))
 }
 
-xgb_plot_df <- process_importance_data(merged_df_all[1:65,], "XGBoost_Importance")
+xgb_plot_df <- process_importance_data(merged_df_all[1:40,], "XGBoost_Importance")
 rf_plot_df  <- process_importance_data(merged_df_all[1:120,], "RandomForest_Importance")
 plot_df_all <- process_importance_data(merged_df_all, "XGBoost_Importance")
 
-#################### preliminary ###########################
+ordered_levels <- c(target_classes, "Other")
 
-## plot p3 with refined origin as shape
-
+xgb_plot_df$display_class <- factor(xgb_plot_df$display_class, levels = ordered_levels)
+rf_plot_df$display_class  <- factor(rf_plot_df$display_class,  levels = ordered_levels)
+plot_df_all$display_class <- factor(plot_df_all$display_class, levels = ordered_levels)
 
 #################### make plots ###########################
 
@@ -82,41 +82,65 @@ p2 <- ggbarplot(rf_plot_df, x = "metabolite", y = "RandomForest_Importance",
 
 
 # for p3 - only label top text
+ordered_levels <- c(target_classes, "Other")
 top <- plot_df_all %>%
   mutate(dist = sqrt(XGBoost_Importance^2 + RandomForest_Importance^2)) %>%
   arrange(desc(dist)) %>%
   slice_head(n = 5) %>%
   pull(metabolite)
 
+active_classes <- ordered_levels[ordered_levels %in% unique(c(
+  as.character(xgb_plot_df$display_class), 
+  as.character(rf_plot_df$display_class)
+))]
+
 p3 <- ggscatter(plot_df_all, 
                 x = "XGBoost_Importance", 
                 y = "RandomForest_Importance",
                 color = "display_class", 
+                shape = "refined_origin", 
                 palette = final_palette,
-                label = "metabolite",      # Column to use for text
-                label.select = top,     # Only show labels for these metabolites
-                repel = TRUE,                     # Prevent text overlap
-                font.label = c(10, "italic"),      # Small, italicized font for formulas
+                label = "metabolite", 
+                label.select = top,
+                repel = TRUE,                     
+                font.label = c(10, "italic"),      
                 cor.coeff = TRUE, 
                 cor.method = "pearson",
                 xlab = "XGBoost Feature Importance", 
                 ylab = "RF Feature Importance") +
   theme_pubr() +
   theme(legend.position = "none")
-# for regression line
-# p3 <- p3 + geom_smooth(method = "lm", color = "steelblue", fill = "#D3d3d3", alpha = 0.2)
 
-# make shared legend
 shared_legend <- get_legend(
-  p1 + theme(legend.position = "right", 
-             legend.text = element_text(size = 8)) +
-    labs(fill = "Compound Superclass") +
-    guides(fill = guide_legend(ncol = 2)) # Set to 2 columns
+  p3 + 
+    scale_color_manual(
+      values = final_palette, 
+      breaks = active_classes, 
+      name = "Compound Superclass"
+    ) +
+    labs(shape = "Metabolite Origin") +
+    theme(legend.position = "right", 
+          legend.text = element_text(size = 8),
+          legend.title = element_text(size = 12)) +
+    guides(
+      color = guide_legend(
+        ncol = 2, 
+        order = 1,
+        override.aes = list(shape = 15, size = 5) # Shape 15 is a solid square
+      ), 
+      shape = guide_legend(
+        ncol = 2, 
+        order = 2,
+        override.aes = list(size = 3) 
+      )
+    )
 )
 
+#make figure
 top_row <- plot_grid(p1, p2, labels = c("A", "B"), ncol = 2)
 bottom_row <- plot_grid(p3, shared_legend, labels = c("C", ""), 
-                        ncol = 2, rel_widths = c(3, 1)) 
+                        ncol = 2, rel_widths = c(1.5, 1)) 
+
 final_fig <- plot_grid(top_row, bottom_row, ncol = 1)
 print(final_fig)
 ggsave("/work/hs325/World_Corals/misc/figs/fip_all.jpg", final_fig, width = 12, height = 6, unit = "in")
